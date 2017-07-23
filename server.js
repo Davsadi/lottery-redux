911dbbd008eb1c8c45199ae5e14a55b1dacae417
David Ferguson <git@dferguson.com>
2017-07-22 23:53:17 -0700

    server.js setup

--- server.js
+++ server.js
@@ -2,16 +2,16 @@ const express = require('express');
 const path = require('path');
 const port = 3007;
 const app = express();
-const root = path.resolve(__dirname, 'relativePath');
+// const root = path.resolve(__dirname, 'relativePath');
 
-// app.use('express.static(__dirname'));
-//
-// app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'index.html'))
-// });
+app.use('express.static(__dirname'));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'index.html'))
+});
 
 
-app.use(express.static(path.resolve(root, '/lottery')));
+// app.use(express.static(path.resolve(root, '/lottery')));
 
 app.listen(port);
 console.log('Server started');
