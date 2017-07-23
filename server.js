bf684b2af3ab403a2b2cb92423e968ddaabf04c4
David Ferguson <git@dferguson.com>
2017-07-23 11:41:01 -0700

    server.js setup

--- server.js
+++ server.js
@@ -3,12 +3,33 @@ const path = require('path');
 const port = 3007;
 const app = express();
 
-app.use('/lottery', express.static('__dirname'));
+// app.use('/lottery', express.static('__dirname'));
 
 // app.get('*', (req, res) => {
 //     res.sendFile(path.resolve(__dirname, 'index.html'))
 // });
 
+
+var sub1 = express();
+sub1.get("/", function(req, res){
+  res.json({status: "SUCCESS!!!!!!"});
+});
+
+var sub2 = express();
+sub2.get("/", function(req, res){
+  res.json({
+    foo: "bar",
+    baz: "quux"
+  });
+});
+
+// main app
+// --------
+
+var app = express();
+
+app.use("/lottery", sub2);
+app.use("/", sub1);
 
 
 app.listen(port);
