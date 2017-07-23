1ddbf184be00a7dd01c375b677600d6ddb7550d0
David Ferguson <git@dferguson.com>
2017-07-22 23:38:29 -0700

    server.js setup

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const port = 3007;
 const app = express();
 
-app.use(express.static(__dirname));
+app.use('/lottery'));
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'index.html'))
