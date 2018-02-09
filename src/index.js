59badbb811a45781f2ba18ba4c2eb689e80ac5e7
David Ferguson <git@dferguson.com>
2018-02-08 21:11:26 -0800

    fixed url path

--- src/index.js
+++ src/index.js
@@ -16,8 +16,8 @@ ReactDOM.render(
         <BrowserRouter>
             <div>
                 <Switch>
-                    <Route path="/lottery/add" component={NewNumbers} />
-                    <Route path="/lottery" component={LotteryIndex} />
+                    <Route path="/add" component={NewNumbers} />
+                    <Route path="/" component={LotteryIndex} />
                 </Switch>
             </div>
         </BrowserRouter>
